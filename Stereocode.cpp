b0d24938d2a18ed10807e5104cd5c53038893724
Jonathan I. Maletic <jmaletic@kent.edu>
2022-06-27 17:27:37 -0400

    Error checking, clean up

--- Stereocode.cpp
+++ Stereocode.cpp
@@ -30,32 +30,36 @@ int main(int argc, char const *argv[])
 
     CLI11_PARSE(app, argc, argv);
 
-    std::string base_directory = "stereocode_tests/HippoDraw-1.11.1/";
-
     std::vector<std::string> file_names_list;
-    if (file_name != "default"){
+    if (file_name != "default") {
         file_names_list.push_back(file_name);
     }
-    else if(file_list != "default"){
+    else if(file_list != "default") {
         file_names_list = readFileNames(file_list);
-    }
-    else{
-        std::cout <<"please select one of the options, -l or -a\n";
-        return -1;
+    } else {
+        std::cout << "Error, incorrect usage\n";
+        std::cout << "   Options: -a input-filename\n";
+        std::cout << "            -l input-list-filename\n";
+        std::cout << "            -o output-filename\n";
+        std::cout << "   Example: stereocode -a foo.xml -o foo.st.xml\n";
+        std::cout << "   Input: srcML archive of foo.hpp and foo.cpp\n";
+      return -1;
     }
     
     std::ofstream output_file;
-    output_file.open("stereotypeReport.txt");
+    output_file.open("stereotypeReport.txt");  //Need to make this variable instead of hard coded.
 
     for (int i = 0; i < file_names_list.size(); ++i){
-        std::string file_path = file_names_list[i];
         srcml_archive* input_archive = srcml_archive_create();
 
-        // read the srcml archive file.
-        // std::cout << "attempting to open file at " << file_path << std::endl;
-        int error = srcml_archive_read_open_filename(input_archive, file_path.c_str());
-        //assumes the hpp file is the first unit.
-        // std::cout << "ERROR OPENING FILE  "<< error << "\n" << std::endl;
+        // read the srcml archive file
+        //assumes the hpp file is the first unit
+
+        int error = srcml_archive_read_open_filename(input_archive, file_names_list[i].c_str());
+        if (error) {
+            std::cout << "Error: file not found: " << file_names_list[i] << ", error == " << error << "\n";
+            return -1;
+        }
 
         srcml_unit* hpp_unit = srcml_archive_read_unit(input_archive);
         srcml_unit* cpp_unit = srcml_archive_read_unit(input_archive);
@@ -63,28 +67,26 @@ int main(int argc, char const *argv[])
 
         //class_representation.print_method_names();
         //class_representation.print_return_types();
-        class_representation.stereotypeGetters(input_archive, hpp_unit, cpp_unit);
-        class_representation.stereotypeSetters(input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeGetters               (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeSetters               (input_archive, hpp_unit, cpp_unit);
         class_representation.stereotypeCollaborationalCommand(input_archive, hpp_unit, cpp_unit);
-        
-        class_representation.stereotypePredicates(input_archive, hpp_unit, cpp_unit);
-        class_representation.stereotypeProperties(input_archive, hpp_unit, cpp_unit);
-        class_representation.stereotypeVoidAccessor(input_archive, hpp_unit, cpp_unit);
-        class_representation.stereotypeCommand(input_archive, hpp_unit, cpp_unit);
-
-        class_representation.stereotypeFactories(input_archive, hpp_unit, cpp_unit);
-        class_representation.stereotypeEmpty(input_archive, hpp_unit, cpp_unit);
-
-        class_representation.stereotypeCollaborators(input_archive, hpp_unit, cpp_unit);
-        class_representation.stereotypeStateless(input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypePredicates            (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeProperties            (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeVoidAccessor          (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeCommand               (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeFactories             (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeEmpty                 (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeCollaborators         (input_archive, hpp_unit, cpp_unit);
+        class_representation.stereotypeStateless             (input_archive, hpp_unit, cpp_unit);
 
         class_representation.printReportToFile(output_file, file_names_list[i]);
 
-        std::cout << "CLASS NAME: \n" << class_representation.get_class_name() << std::endl;
+        std::cout << "Class name: " << class_representation.get_class_name() << std::endl;
         //class_representation.printMethodHeaders();
         //class_representation.printStereotypes();
-        class_representation.printAttributes();
+        //class_representation.printAttributes();
         //class_representation.printReturnTypes();
+        
         if (class_representation.get_inline_function_count() != 0) {
             hpp_unit = class_representation.writeStereotypeAttribute(input_archive, hpp_unit, true);
         }
@@ -95,7 +97,7 @@ int main(int argc, char const *argv[])
         srcml_archive* output_archive = srcml_archive_create();
 
         error = srcml_archive_write_open_filename(output_archive, (file_names_list[i] + ".annotated.xml").c_str());
-        if (error != 0) {
+        if (error) {
             std::cout << "error opening " << ( "" + file_names_list[i] + ".annotated.xml") << std::endl;
             return -1;
         }
@@ -110,10 +112,13 @@ int main(int argc, char const *argv[])
         srcml_archive_free(input_archive);
     }
     output_file.close();
+    std::cout << "Stereotyping completed." << std::endl;
 
     return 0;
 }
 
+//
+//
 std::vector<std::string> readFileNames(const std::string & file_name){
     std::vector<std::string> list_of_archives;
     std::ifstream archives_file;
@@ -121,13 +126,11 @@ std::vector<std::string> readFileNames(const std::string & file_name){
 
     if (archives_file.is_open()){
         std::string name;
-
         while(std::getline(archives_file, name)){
             list_of_archives.push_back(name);
         }
-    }
-    else{
-        std::cout << "unable to open file containing archive names\n";
+    } else{
+        std::cout << "Error: file not found: " << file_name << "\n";
     }
     return list_of_archives;
 }
